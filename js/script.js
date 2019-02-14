c0d04309f066b58aae2d9269bdd47cfcb7b1d558
claudio scotto <claudiosdv@gmail.com>
2019-02-14 13:42:15 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,27 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
 
 
+
+// controls
+
+				controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+				//controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+				controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+				controls.dampingFactor = 0.25;
+
+				controls.screenSpacePanning = false;
+
+				controls.minDistance = 100;
+				controls.maxDistance = 500;
+
+				controls.maxPolarAngle = Math.PI / 2;
+
+
+
+
+
 window.addEventListener( 'resize', onWindowResize, false );
 
 function onWindowResize(){
