a77f12b579cdc0c24c21c0d161500ad52d4d97f4
claudio scotto <claudiosdv@gmail.com>
2019-02-14 16:05:50 +0000

    7

--- js/script.js
+++ js/script.js
@@ -60,56 +60,24 @@ function sceneSetup() {
 	scene.add( sphere );
 	scene.add( plane );
 
-
- // texture
-  var manager = new THREE.LoadingManager();
-  manager.onProgress = function(item, loaded, total) {
-
-    console.log(item, loaded, total);
-
-  };
-
-  var texture = new THREE.Texture();
-
-  var onProgress = function(xhr) {
-    if (xhr.lengthComputable) {
-      var percentComplete = xhr.loaded / xhr.total * 100;
-      console.log(Math.round(percentComplete, 2) + '% downloaded');
-    }
-  };
-
-  var onError = function(xhr) {};
-
-  var loader = new THREE.ImageLoader(manager);
-   loader.load('image.png', function(image) {
-
-    texture.image = image;
-    texture.needsUpdate = true;
-
-  });
-
-  // model
-  var loader = new THREE.OBJLoader(manager);
-  loader.load('obj/Desk.obj', function(object) {
-
-    object.traverse(function(child) {
-
-      if (child instanceof THREE.Mesh) {
-
-        child.material.map = texture;
-
-      }
-
-    });
-
-    object.scale.x = 45;
-    object.scale.y = 45;
-    object.scale.z = 45;
-    object.rotation.y = 3;
-    object.position.y = -10.5;
-    scene.add(object);
-
-  }, onProgress, onError);
+		var mtlLoader = new THREE.MTLLoader();
+		mtlLoader.setTexturePath('obj/');
+		mtlLoader.setPath('obj/');
+		mtlLoader.load('Desk.mtl', function (materials) {
+		 
+		    materials.preload();
+		 
+		    var objLoader = new THREE.OBJLoader();
+		    objLoader.setMaterials(materials);
+		    objLoader.setPath('obj/');
+		    objLoader.load('Desk.obj', function (myobject) {
+		 
+		        scene.add(myobject);
+		        myobject.position.y = 2;
+		 
+		    });
+		 
+		});
 
 	function animateElem() {
 	cube.rotation.x += 0.01;
