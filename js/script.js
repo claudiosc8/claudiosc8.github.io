d55b1e52cd03297e0e91ab0c6cbb91f9f64b7ecd
claudio scotto <claudiosdv@gmail.com>
2019-02-14 13:24:21 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,24 +80,13 @@ animate();
 
 var loader = new THREE.ObjectLoader();
 
-loader.load(
-	// resource URL
-	"models/json/example.json",
+var loader = new THREE.JSONLoader();
+        loader.load( 'js/scene.json', function ( geometry ) {
+        var mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial() );
 
-	// onLoad callback
-	// Here the loaded data is assumed to be an object
-	function ( obj ) {
-		// Add the loaded object to the scene
-		scene.add( obj );
-	},
+                        mesh.position.x =500;
+                        mesh.position.y =100;
+                        mesh.position.z =500;
+        scene.add( mesh );
 
-	// onProgress callback
-	function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-	},
-
-	// onError callback
-	function ( err ) {
-		console.error( 'An error happened' );
-	}
-);
+        }); 
