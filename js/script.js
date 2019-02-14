24e40ee3672050420dd2f678082b0aeb16b6850d
claudio scotto <claudiosdv@gmail.com>
2019-02-14 15:06:54 +0000

    asd

--- js/script.js
+++ js/script.js
@@ -1,33 +1,104 @@
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+let camera, scene, renderer;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
-
-
-
-// controls
-
-				controls = new THREE.OrbitControls( camera, renderer.domElement );
-
-				//controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
-
-				controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-				controls.dampingFactor = 0.25;
-
-				controls.screenSpacePanning = false;
-
-				controls.minDistance = 100;
-				controls.maxDistance = 500;
-
-				controls.maxPolarAngle = Math.PI / 2;
+function init() {
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight ); 
+	document.body.appendChild( renderer.domElement );
+	renderer.shadowMap.enabled = true;  // enable Shadows
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = 25;
+	// controls
+	controls = new THREE.OrbitControls( camera, renderer.domElement );
+	controls.enableDamping = true; 
+	controls.dampingFactor = 0.25;
+	window.addEventListener( 'resize', onWindowResize, false );
+}
 
 
+function sceneSetup() {
+	//Create a PointLight and turn on shadows for the light
+	var light = new THREE.PointLight( 0xffffff, 1, 100 );
+	light.position.set( 0, 10, 5 );
+	light.castShadow = true;            // default false
+	scene.add( light );
+	scene.add( new THREE.AmbientLight( 0xff0000 ));
+
+	var cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 ).translate(3,0,0);
+	var sphereGeometry = new THREE.SphereGeometry( 2, 12, 12 );
+	var planegeometry = new THREE.BoxGeometry( 50, 50 , 0.1 );
+
+
+	var one = new THREE.MeshPhongMaterial( { color: 0x4080ff} );
+	var two = new THREE.MeshPhongMaterial( { color: 0x4080ff, flatShading:true } );
+
+	var cube = new THREE.Mesh( cubeGeometry, one );
+	var sphere = new THREE.Mesh( sphereGeometry, two );
+	var plane = new THREE.Mesh( planegeometry, one );
+
+
+// manager
+				function loadModel() {
+					object.traverse( function ( child ) {
+						if ( child.isMesh ) child.material.map = texture;
+					} );
+					object.position.y = - 2;
+					scene.add( object );
+				}
+				var manager = new THREE.LoadingManager( loadModel );
+				manager.onProgress = function ( item, loaded, total ) {
+					console.log( item, loaded, total );
+				};
+				// texture
+				var textureLoader = new THREE.TextureLoader( manager );
+				var texture = textureLoader.load( 'onj.jpg' );
+				// model
+				function onProgress( xhr ) {
+					if ( xhr.lengthComputable ) {
+						var percentComplete = xhr.loaded / xhr.total * 100;
+						console.log( 'model ' + Math.round( percentComplete, 2 ) + '% downloaded' );
+					}
+				}
+				function onError() {}
+				var loader = new THREE.OBJLoader( manager );
+				loader.load( 'obj/desk.obj', function ( obj ) {
+					object = obj;
+				}, onProgress, onError );
+				//
 
 
 
-window.addEventListener( 'resize', onWindowResize, false );
+	sphere.castShadow = true; 
+	sphere.receiveShadow = true; 
+	cube.castShadow = true; 
+	cube.receiveShadow = true; 
+	plane.castShadow = true; 
+	plane.receiveShadow = true; 
+
+	function rotateObject(object, degreeX=0, degreeY=0, degreeZ=0) {
+	   object.rotateX(THREE.Math.degToRad(degreeX));
+	   object.rotateY(THREE.Math.degToRad(degreeY));
+	   object.rotateZ(THREE.Math.degToRad(degreeZ));
+	}
+
+	rotateObject(plane, 0, 0, 0);
+	plane.position.set(0,-2,-3);
+
+	scene.add( cube );
+	scene.add( sphere );
+	scene.add( plane );
+
+
+
+
+	function animateElem() {
+	cube.rotation.x += 0.01;
+	sphere.rotation.y += 0.01;
+	}
+
+	animateElem();
+}
 
 function onWindowResize(){
 
@@ -38,74 +109,15 @@ function onWindowResize(){
 
 }
 
-scene.add( new THREE.AmbientLight( 0x666666 ) );
-				var light = new THREE.DirectionalLight( 0xdfebff, 1 );
-				light.position.set( 3, 3, 2 );
-				light.position.multiplyScalar( 1.3 );
-				light.castShadow = true;
-				light.shadow.mapSize.width = 1024;
-				light.shadow.mapSize.height = 1024;
-				var d = 300;
-				light.shadow.camera.left = - d;
-				light.shadow.camera.right = d;
-				light.shadow.camera.top = d;
-				light.shadow.camera.bottom = - d;
-				light.shadow.camera.far = 1000;
-	
-
-var cubemesh = new THREE.BoxGeometry( 1, 1, 1 ).translate(3,0,0);
-var sphere = new THREE.SphereGeometry( 2, 12, 12 );
-var planegeometry = new THREE.BoxGeometry( 50, 50 , 0.1 );
-cubemesh.receiveShadow = true;
-sphere.receiveShadow = true;
-cubemesh.castShadow = true;
-sphere.castShadow = true;
-var green = new THREE.MeshPhongMaterial( { color: 0x4080ff, dithering: true } );
-var red = new THREE.MeshPhongMaterial( { color: 0x4080ff, dithering: false, flatShading:true } );
-var cube = new THREE.Mesh( cubemesh, green );
-var sphere = new THREE.Mesh( sphere, red );
-var plane = new THREE.Mesh( planegeometry, red );
-
-plane.receiveShadow = true;
-
-
-function rotateObject(object, degreeX=0, degreeY=0, degreeZ=0) {
-   object.rotateX(THREE.Math.degToRad(degreeX));
-   object.rotateY(THREE.Math.degToRad(degreeY));
-   object.rotateZ(THREE.Math.degToRad(degreeZ));
-}
-
-// usage:
-rotateObject(plane, 0, 0, 0);
-plane.position.set(0,-2,-10);
-
-scene.add( cube );
-scene.add( sphere );
-scene.add( plane );
-scene.add( light );
-
-camera.position.z = 5;
-
-
 function animate() {
 	requestAnimationFrame( animate );
-
-	controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
-
+	controls.update();
 	renderer.render( scene, camera );
-  cube.rotation.x += 0.01;
-sphere.rotation.y += 0.01;
-
-  
 }
+
+init();
+sceneSetup();
 animate();
 
 
 
-
-// BEGIN Clara.io JSON loader code
-var objectLoader = new THREE.ObjectLoader();
-objectLoader.load("js/jscene.json", function ( obj ) {
-scene.add( obj );
-} );
-// END Clara.io JSON loader code
