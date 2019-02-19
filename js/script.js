90c4036ea91cac4fac15661cfdace6e5c19f6465
Claudio Scotto <claudiosdv@gmail.com>
2019-02-19 20:21:57 +0000

    a

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let camera, scene, renderer;
+let camera, scene, renderer, player;
 
 function init() {
 	renderer = new THREE.WebGLRenderer();
@@ -6,45 +6,55 @@ function init() {
 	document.body.appendChild( renderer.domElement );
 	renderer.shadowMap.enabled = true;  // enable Shadows
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+	renderer.gammaOutput = true;
+	renderer.gammaFactor = 2.2;
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
-	camera.position.z = 25;
+	camera.position.set(0,3,5);
 	// controls
 	controls = new THREE.OrbitControls( camera, renderer.domElement );
-	controls.enableDamping = true; 
-	controls.dampingFactor = 0.25;
+	// controls.enableDamping = true; 
+	// controls.dampingFactor = 0;
+	controls.screenSpacePanning = false;
 	window.addEventListener( 'resize', onWindowResize, false );
+
+	
 }
 
 
 function sceneSetup() {
-	//Create a PointLight and turn on shadows for the light
-	var light = new THREE.PointLight( 0xffffff, 1, 100 );
-	light.position.set( 0, 10, 5 );
-	light.castShadow = true;            // default false
+	// lights
+
+	var light = new THREE.DirectionalLight( 0xffffff );
+	light.position.set( 0, 20, 10 );
+	light.castShadow = true;
 	scene.add( light );
-	scene.add( new THREE.AmbientLight( 0xff0000 ));
 
-	var cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 ).translate(3,0,0);
-	var sphereGeometry = new THREE.SphereGeometry( 2, 12, 12 );
-	var planegeometry = new THREE.BoxGeometry( 50, 50 , 0.1 );
+	light = new THREE.AmbientLight( 0x888888);
+	light.position.set( 0, 20, 0 );
+	scene.add( light );
 
-
-	var one = new THREE.MeshPhongMaterial( { color: 0x4080ff} );
-	var two = new THREE.MeshPhongMaterial( { color: 0x4080ff, flatShading:true } );
-
-	var cube = new THREE.Mesh( cubeGeometry, one );
-	var sphere = new THREE.Mesh( sphereGeometry, two );
-	var plane = new THREE.Mesh( planegeometry, one );
+	var cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var sphereGeometry = new THREE.SphereGeometry( 1, 12, 12 );
+	var planegeometry = new THREE.PlaneGeometry( 100, 100, 100, 100);
 
 
+	var mone = new THREE.MeshPhongMaterial( { color: 0x4080ff} );
+	var mtwo = new THREE.MeshPhongMaterial( { color: 0xde2301, flatShading:true } );
+	var mthree = new THREE.MeshPhongMaterial( { color: 0xff0009} );
+
+	var cube = new THREE.Mesh( cubeGeometry, mthree );
+	var player = new THREE.Mesh( sphereGeometry, mtwo );
+	var plane = new THREE.Mesh( planegeometry, mone );
+
+	plane.material.side = THREE.DoubleSide;
    
 
-	sphere.castShadow = true; 
-	sphere.receiveShadow = true; 
+	player.castShadow = true; 
+	player.receiveShadow = true; 
 	cube.castShadow = true; 
 	cube.receiveShadow = true; 
-	plane.castShadow = true; 
+
 	plane.receiveShadow = true; 
 
 	function rotateObject(object, degreeX=0, degreeY=0, degreeZ=0) {
@@ -53,71 +63,75 @@ function sceneSetup() {
 	   object.rotateZ(THREE.Math.degToRad(degreeZ));
 	}
 
-	rotateObject(plane, 0, 0, 0);
-	plane.position.set(0,-2,-3);
+	rotateObject(plane, 90, 0, 0);
+	plane.position.set(0,0,0);
+	player.position.set(0,4,0);
+	cube.position.set(2,4,0);
+	// var helper = new THREE.GridHelper( 1000, 200, 0xFF4444, 0x404040 );
+	// scene.add( helper );
 
 	scene.add( cube );
-	scene.add( sphere );
+	scene.add( player );
 	scene.add( plane );
 
-		// manager
-				function loadModel() {
-					object.traverse( function ( child ) {
-						if ( child.isMesh ) child.material.map = texture;
-					} );
-					object.position.y = - 2;
-					scene.add( object );
-				}
-				var manager = new THREE.LoadingManager( loadModel );
-				manager.onProgress = function ( item, loaded, total ) {
-					console.log( item, loaded, total );
-				};
-				// texture
-				var textureLoader = new THREE.TextureLoader( manager );
-				var texture = textureLoader.load( 'object/UV_Grid_Sm.jpg' );
-				// model
-				function onProgress( xhr ) {
-					if ( xhr.lengthComputable ) {
-						var percentComplete = xhr.loaded / xhr.total * 100;
-						console.log( 'model ' + Math.round( percentComplete, 2 ) + '% downloaded' );
-					}
-				}
-				function onError() {}
-				var loader = new THREE.OBJLoader( manager );
-				loader.load( 'object/male02.obj', function ( obj ) {
-					object = obj;
-				}, onProgress, onError );
-				//
+
+	// model
 
 
+	function error ( error ) {console.error( error );}
 
-	function animateElem() {
-	cube.rotation.x += 0.01;
-	sphere.rotation.y += 0.01;
-	}
+	var loader = new THREE.GLTFLoader();
 
-	animateElem();
+	loader.load( 'object/desk.glb', function ( gltf ) {
+
+		gltf.scene.traverse( function( obj ) {
+	        if ( obj instanceof THREE.Mesh ) {
+	         obj.castShadow = true; 
+	         obj.receiveShadow = true; 
+	         obj.scale.set(12,12,12);
+			obj.position.set(0,0.6,0);
+			}
+	    } );
+
+		scene.add( gltf.scene );
+
+	}, undefined, error );
+
+
+	loader.load( 'object/astro.glb', function ( gltf ) {
+
+		gltf.scene.traverse( function( obj ) {
+	        if ( obj instanceof THREE.Mesh ) {
+	        	obj.castShadow = true; 
+	       	  	obj.receiveShadow = true; 
+	       	  	obj.scale.set(1,1,1);
+				obj.position.set(-2,0,0);
+			}
+	    } );
+
+		scene.add( gltf.scene );
+
+	}, undefined, error );
+
 
 }
 
 function onWindowResize(){
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
-
+				
 function animate() {
 	requestAnimationFrame( animate );
 	controls.update();
+	render();
+}
+
+function render() {
 	renderer.render( scene, camera );
 }
 
 init();
 sceneSetup();
 animate();
-
-
-
